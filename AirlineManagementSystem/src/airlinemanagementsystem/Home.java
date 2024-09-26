013c0e007ada2ba3e58c7d108a219010bfb3f318
falkenbeast <arshayush21@gmail.com>
2024-09-26 22:37:14 +0530

    changes

---
+++ AirlineManagementSystem/src/airlinemanagementsystem/Home.java
@@ -0,0 +1,63 @@
+package airlinemanagementsystem;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class Home extends JFrame implements ActionListener{
+    public Home(){
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/mainf.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1800, 800);
+        add(image);
+        
+        JLabel heading = new JLabel("Hi there, where would you like to IndiGo today?");
+        heading.setBounds(400, 40, 1000, 40);
+         heading.setForeground(new Color(32,23,22));
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 36));
+        image.add(heading);
+        
+        JMenuBar menubar = new JMenuBar();
+        setJMenuBar(menubar);
+        
+        JMenu details = new JMenu("Details");
+        menubar.add(details);
+        
+        
+       JMenuItem flightDetails = new JMenuItem("Flight Details");
+        flightDetails.addActionListener(this);
+        details.add(flightDetails);
+        
+        JMenuItem customerDetails = new JMenuItem("Add Customer Details");
+        customerDetails.addActionListener(this);
+        details.add(customerDetails);
+        
+        JMenuItem bookFlight = new JMenuItem("Book Flight");
+        bookFlight.addActionListener(this);
+        details.add(bookFlight);
+        
+        JMenuItem journeyDetails = new JMenuItem("Journey Details");
+        journeyDetails.addActionListener(this);
+        details.add(journeyDetails);
+        
+        JMenuItem ticketCancellation = new JMenuItem("Cancel Ticket");
+        ticketCancellation.addActionListener(this);
+        details.add(ticketCancellation);
+        
+        
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setLocation(600, 250);
+        setVisible(true);
+    }
+    
+  public void actionPerformed(ActionEvent ae) {
+    
+  }
+    
+    public static void main(String[] args) {
+        new Home();
+    }
+}
